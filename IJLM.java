e7022d494c223c240384bb1d7a72f9f922438e46
nacho-luengo-jgv <luengomoreno.ignaciojoel@jgvaldemora.org>
2025-04-01 10:03:58 +0200

    He creado otra clase con las iniciales y la he reemplazado

--- IJLM.java
+++ IJLM.java
@@ -11,7 +11,6 @@ public class U03_A3 {
         int numero =sc.nextInt();
         numero++;
         System.out.println("La edad que vas a tener el proximo anio es:"+numero);
-	System.out.println("Nueva linea");
         
 }
      
