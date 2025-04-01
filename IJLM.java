df28ff1193d54a0c98efc992ac29b70091800e69
nacho-luengo-jgv <luengomoreno.ignaciojoel@jgvaldemora.org>
2025-04-01 09:19:23 +0200

    Añadi un trabajo de la unidad tres llamado U03_A3 y lo renombre con mis iniciales

---
+++ IJLM.java
@@ -0,0 +1,17 @@
+
+package unidad3;
+import java.util.Scanner;
+
+public class U03_A3 {
+    
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Introduce tu edad: ");
+        int numero =sc.nextInt();
+        numero++;
+        System.out.println("La edad que vas a tener el proximo anio es:"+numero);
+        
+}
+     
+}
